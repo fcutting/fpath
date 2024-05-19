c917a2d9c7c904bb8bfa7be4e068aca5c08e0b83
FletcherCutting <admin@fletchercutting.au>
2024-05-19 21:52:30 +1000

    Renamed bufferedRuneReader to tokenReader

--- bufferedRuneReader.go
+++ bufferedRuneReader.go
@@ -1,30 +1,30 @@
 package fpath
 
-// bufferedRuneReader adds the functionality to get and peek runes from a
+// tokenReader adds the functionality to get and peek tokens from a
 // string using a buffer.
-type bufferedRuneReader struct {
+type tokenReader struct {
 	input []rune
 	index int
 }
 
-// newBufferedRuneReader returns a new bufferedRuneReader configured to read
-// from a []rune value of the input string.
-func newBufferedRuneReader(input string) (brr *bufferedRuneReader) {
-	return &bufferedRuneReader{
+// newTokenReader returns a new bufferedRuneReader configured to read from a
+// []rune value of the input string.
+func newTokenReader(input string) (tr *tokenReader) {
+	return &tokenReader{
 		input: []rune(input),
 	}
 }
 
 // Get returns the rune at the current index of the input and increments the
 // index.
-func (brr *bufferedRuneReader) get() (r rune) {
-	r = brr.input[brr.index]
-	brr.index++
+func (tr *tokenReader) get() (r rune) {
+	r = tr.input[tr.index]
+	tr.index++
 	return r
 }
 
 // Peek returns the rune at the current index of the input but doesn't
 // increment the index.
-func (brr *bufferedRuneReader) peek() (r rune) {
-	return brr.input[brr.index]
+func (tr *tokenReader) peek() (r rune) {
+	return tr.input[tr.index]
 }
--- bufferedRuneReader_test.go
+++ bufferedRuneReader_test.go
@@ -5,10 +5,10 @@ import "testing"
 func Test_bufferedRuneReader_get(t *testing.T) {
 	input := "hello world"
 	expectedRunes := []rune{'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'}
-	brr := newBufferedRuneReader(input)
+	tr := newTokenReader(input)
 
 	for i, expected := range expectedRunes {
-		r := brr.get()
+		r := tr.get()
 		if r != expected {
 			t.Fatalf("Unexpected rune at position %d\nExpected: %q\n Actual: %q", i, expected, r)
 		}
@@ -18,9 +18,9 @@ func Test_bufferedRuneReader_get(t *testing.T) {
 func Test_bufferedRuneReader_peek(t *testing.T) {
 	input := "hello world"
 	expected := 'h'
-	brr := newBufferedRuneReader(input)
+	tr := newTokenReader(input)
 	for i := 0; i < 10; i++ {
-		r := brr.peek()
+		r := tr.peek()
 		if r != expected {
 			t.Fatalf("Unexpected rune\nExpected: %q\nActual: %q", expected, r)
 		}
