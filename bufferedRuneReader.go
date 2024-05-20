02d9f575092bc2e221923d8202f8376f0ab38958
FletcherCutting <admin@fletchercutting.au>
2024-05-20 21:01:34 +1000

    Add EOF at end of input

--- bufferedRuneReader.go
+++ bufferedRuneReader.go
@@ -1,5 +1,19 @@
 package fpath
 
+import (
+	"io"
+)
+
+const (
+	TokenType_Undefined = iota
+	TokenType_Number
+)
+
+type token struct {
+	typ   int
+	value string
+}
+
 // tokenReader adds the functionality to get and peek tokens from a
 // string using a buffer.
 type tokenReader struct {
@@ -17,14 +31,22 @@ func newTokenReader(input string) (tr *tokenReader) {
 
 // Get returns the rune at the current index of the input and increments the
 // index.
-func (tr *tokenReader) getRune() (r rune) {
+func (tr *tokenReader) getRune() (r rune, err error) {
+	if tr.index == len(tr.input) {
+		return 0, io.EOF
+	}
+
 	r = tr.input[tr.index]
 	tr.index++
-	return r
+	return r, nil
 }
 
 // Peek returns the rune at the current index of the input but doesn't
 // increment the index.
-func (tr *tokenReader) peekRune() (r rune) {
-	return tr.input[tr.index]
+func (tr *tokenReader) peekRune() (r rune, err error) {
+	if tr.index == len(tr.input) {
+		return 0, io.EOF
+	}
+
+	return tr.input[tr.index], nil
 }
--- bufferedRuneReader_test.go
+++ bufferedRuneReader_test.go
@@ -1,28 +1,86 @@
 package fpath
 
-import "testing"
+import (
+	"io"
+	"testing"
+)
 
-func Test_tokenReader_get(t *testing.T) {
+func Test_tokenReader_getRune(t *testing.T) {
 	input := "hello world"
 	expectedRunes := []rune{'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'}
 	tr := newTokenReader(input)
 
 	for i, expected := range expectedRunes {
-		r := tr.getRune()
+		r, err := tr.getRune()
+
+		if err != nil {
+			t.Fatalf("Unexpected error: %s", err)
+		}
+
 		if r != expected {
 			t.Fatalf("Unexpected rune at position %d\nExpected: %q\n Actual: %q", i, expected, r)
 		}
 	}
 }
 
-func Test_tokenReader_peek(t *testing.T) {
+func Test_tokenReader_getRune_EOF(t *testing.T) {
+	input := "h"
+	expected := 'h'
+	tr := newTokenReader(input)
+
+	r, err := tr.getRune()
+
+	if err != nil {
+		t.Fatalf("Unexpected error: %s", err)
+	}
+
+	if r != expected {
+		t.Fatalf("Unexpected rune\nExpected: %q\nActual: %q", expected, r)
+	}
+
+	for i := 0; i < 100; i++ {
+		if _, err = tr.getRune(); err != io.EOF {
+			t.Fatalf("Unexpected error: %s", err)
+		}
+	}
+}
+
+func Test_tokenReader_peekRune(t *testing.T) {
 	input := "hello world"
 	expected := 'h'
 	tr := newTokenReader(input)
+
 	for i := 0; i < 10; i++ {
-		r := tr.peekRune()
+		r, err := tr.peekRune()
+
+		if err != nil {
+			t.Fatalf("Unexpected error: %s", err)
+		}
+
 		if r != expected {
 			t.Fatalf("Unexpected rune\nExpected: %q\nActual: %q", expected, r)
+		}
+	}
+}
+
+func Test_tokenReader_peekRune_EOF(t *testing.T) {
+	input := "h"
+	expected := 'h'
+	tr := newTokenReader(input)
+
+	r, err := tr.getRune()
+
+	if err != nil {
+		t.Fatalf("Unexpected error: %s", err)
+	}
+
+	if r != expected {
+		t.Fatalf("Unexpected rune\nExpected: %q\nActual: %q", expected, r)
+	}
+
+	for i := 0; i < 100; i++ {
+		if _, err = tr.peekRune(); err != io.EOF {
+			t.Fatalf("Unexpected error: %s", err)
 		}
 	}
 }
